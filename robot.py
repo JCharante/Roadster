6d76d750ea30cfe74f3b4a3ca6a61ea85611e318
JCharante <git@jcharante.com>
2017-05-19 05:23:36 -0400

    Now indenting correctly (with tabs)

--- robot.py
+++ robot.py
@@ -5,26 +5,26 @@ from utils.gamepad import Gamepad
 
 
 class MyRobot(wpilib.IterativeRobot):
-    logger = logging.getLogger("robot")
+	logger = logging.getLogger("robot")
 
-    def robotInit(self):
-        # object that handles basic drive operations
-        self.myRobot = wpilib.RobotDrive(0, 1)
-        self.myRobot.setExpiration(0.1)
+	def robotInit(self):
+		# object that handles basic drive operations
+		self.myRobot = wpilib.RobotDrive(0, 1)
+		self.myRobot.setExpiration(0.1)
 
-        # joysticks 1 & 2 on the driver station
-        self.gamepad = Gamepad(0)
-        #self.operatorConsole = wpilib.Joystick(1)
+		# joysticks 1 & 2 on the driver station
+		self.gamepad = Gamepad(0)
+		# self.operatorConsole = wpilib.Joystick(1)
 
-    def teleopInit(self):
-        self.myRobot.setSafetyEnabled(True)
-        self.logger.warning('You have warnings logs on')
-        self.logger.info('You have info logs on')
+	def teleopInit(self):
+		self.myRobot.setSafetyEnabled(True)
+		self.logger.warning('You have warnings logs on')
+		self.logger.info('You have info logs on')
 
-    def teleopPeriodic(self):
-        self.logger.info(f'Gamepad Left X: {self.gamepad.LEFT_X.val(2)}, Y: {self.gamepad.LEFT_Y.val(2)} | Gamepad Right X: {self.gamepad.RIGHT_X.val(2)}, Y: {self.gamepad.RIGHT_Y.val(2)}')
-        #self.myRobot.tankDrive(self.leftStick, self.rightStick, True)
+	def teleopPeriodic(self):
+		self.logger.info(f'Gamepad Left X: {self.gamepad.LEFT_X.val(2)}, Y: {self.gamepad.LEFT_Y.val(2)} | Gamepad Right X: {self.gamepad.RIGHT_X.val(2)}, Y: {self.gamepad.RIGHT_Y.val(2)}')
+		# self.myRobot.tankDrive(self.leftStick, self.rightStick, True)
 
 
 if __name__ == '__main__':
-    wpilib.run(MyRobot)
+	wpilib.run(MyRobot)
--- utils/gamepad.py
+++ utils/gamepad.py
@@ -2,38 +2,35 @@ import wpilib
 
 
 class GamepadAxis:
+	def __init__(self, gamepad: 'Gamepad', axis: int, invert=False):
+		self.gamepad = gamepad
+		self.axis = axis
+		self.invert = invert
 
-    def __init__(self, gamepad: 'Gamepad', axis: int, invert=False):
-        self.gamepad = gamepad
-        self.axis = axis
-        self.invert = invert
-
-    def val(self, decimal_places=None):
-        if self.invert:
-            return self.gamepad.getRawAxis(self.axis) * -1 if round is None else round(self.gamepad.getRawAxis(self.axis) * -1, decimal_places)
-        else:
-            return self.gamepad.getRawAxis(self.axis) if round is None else round(self.gamepad.getRawAxis(self.axis), decimal_places)
+	def val(self, decimal_places=None):
+		if self.invert:
+			return self.gamepad.getRawAxis(self.axis) * -1 if round is None else round(self.gamepad.getRawAxis(self.axis) * -1, decimal_places)
+		else:
+			return self.gamepad.getRawAxis(self.axis) if round is None else round(self.gamepad.getRawAxis(self.axis), decimal_places)
 
 
 class GamepadButton:
+	def __init__(self, gamepad: 'Gamepad', button_number: int):
+		self.gamepad = gamepad
+		self.button_number = button_number
 
-    def __init__(self, gamepad: 'Gamepad', button_number: int):
-        self.gamepad = gamepad
-        self.button_number = button_number
-
-    def isPressed(self):
-        return self.gamepad.getRawButton(self.button_number)
+	def isPressed(self):
+		return self.gamepad.getRawButton(self.button_number)
 
 
 class Gamepad(wpilib.Joystick):
-
-    def __init__(self, port: int):
-        super().__init__(port)
-        self.LEFT_X = GamepadAxis(self, 0)
-        self.LEFT_Y = GamepadAxis(self, 1, True)
-        self.LEFT_TRIGGER = GamepadAxis(self, 2)
-        self.RIGHT_TRIGGER = GamepadAxis(self, 3)
-        self.RIGHT_X = GamepadAxis(self, 4)
-        self.RIGHT_Y = GamepadAxis(self, 5, True)
-        self.D_PAD_HORIZONTAL = GamepadAxis(self, 6)
-        self.D_PAD_VERTICAL = GamepadAxis(self, 7)
+	def __init__(self, port: int):
+		super().__init__(port)
+		self.LEFT_X = GamepadAxis(self, 0)
+		self.LEFT_Y = GamepadAxis(self, 1, True)
+		self.LEFT_TRIGGER = GamepadAxis(self, 2)
+		self.RIGHT_TRIGGER = GamepadAxis(self, 3)
+		self.RIGHT_X = GamepadAxis(self, 4)
+		self.RIGHT_Y = GamepadAxis(self, 5, True)
+		self.D_PAD_HORIZONTAL = GamepadAxis(self, 6)
+		self.D_PAD_VERTICAL = GamepadAxis(self, 7)
