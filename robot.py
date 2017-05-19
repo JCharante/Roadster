21ea6e0e5b196d9776fbd4c6165f27e9078de98c
JCharante <git@jcharante.com>
2017-05-19 05:16:58 -0400

    Added gamepad abstractor

--- robot.py
+++ robot.py
@@ -1,6 +1,8 @@
 import wpilib
 import logging
 
+from utils.gamepad import Gamepad
+
 
 class MyRobot(wpilib.IterativeRobot):
     logger = logging.getLogger("robot")
@@ -11,7 +13,8 @@ class MyRobot(wpilib.IterativeRobot):
         self.myRobot.setExpiration(0.1)
 
         # joysticks 1 & 2 on the driver station
-        self.gamepad = wpilib.Joystick(0, 6)
+        self.gamepad = Gamepad(0)
+        #self.operatorConsole = wpilib.Joystick(1)
 
     def teleopInit(self):
         self.myRobot.setSafetyEnabled(True)
@@ -19,11 +22,7 @@ class MyRobot(wpilib.IterativeRobot):
         self.logger.info('You have info logs on')
 
     def teleopPeriodic(self):
-        xbox_stick_left_x = round(self.gamepad.getRawAxis(0), 2)
-        xbox_stick_left_y = round(self.gamepad.getRawAxis(1), 2)
-        xbox_stick_right_x = round(self.gamepad.getRawAxis(4), 2)
-        xbox_stick_right_y = round(self.gamepad.getRawAxis(5), 2)
-        self.logger.info(f'Gamepad Left X: {xbox_stick_left_x}, Y: {xbox_stick_left_y} | Gamepad Right X: {xbox_stick_right_x}, Y: {xbox_stick_right_y}')
+        self.logger.info(f'Gamepad Left X: {self.gamepad.LEFT_X.val(2)}, Y: {self.gamepad.LEFT_Y.val(2)} | Gamepad Right X: {self.gamepad.RIGHT_X.val(2)}, Y: {self.gamepad.RIGHT_Y.val(2)}')
         #self.myRobot.tankDrive(self.leftStick, self.rightStick, True)
 
 
